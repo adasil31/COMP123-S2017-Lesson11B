711ff521516c63324caf95967294485e3bdcec6a
ttt <ttt@ALE-554RL5K>
2017-07-27 12:58:02 -0400

    COMP123-S2017-Lesson11B

---
+++ COMP123-S2017-Lesson11B/CardList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_S2017_Lesson11B
+{
+    public abstract class CardList
+    {
+        //private instance viable
+
+        // public properties 
+
+
+        //
+    }
+}
--- COMP123-S2017-Lesson11B/Program.cs
+++ COMP123-S2017-Lesson11B/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 25, 2017
+ * Description: This is a demo for Lesson 11
+ * Version: 0.3 - Test Shuffle method of the Deck class
+ */
+
 namespace COMP123_S2017_Lesson11B
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Deck deck = new Deck();
+            Console.WriteLine("Original Deck   Number of Cards: " + deck.Count);
+            Console.WriteLine("==================================");
+            Console.WriteLine(deck.ToString());
+            Console.WriteLine();
+            deck.Shuffle();
+            Console.WriteLine("Shuffled Deck   Number of Cards: " + deck.Count);
+            Console.WriteLine("==================================");
+            Console.WriteLine(deck.ToString());
         }
     }
 }
